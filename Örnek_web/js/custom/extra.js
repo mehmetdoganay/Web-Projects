09d6ed5493b1d3275dbde4a04391498163eb4b02
Mehmet Doganay <mehmetdoganay@Mehmet-MacBook-Air.local>
2022-07-24 14:44:55 +0300

    add örnek-web

---
+++ Örnek_web/js/custom/extra.js
@@ -0,0 +1,43 @@
+
+
+//html ögeleri yüklendiğinde navbarNobile() fonksiyonunu çalıştır.
+window.addEventListener("DOMContentLoaded", event => {
+    var navbarMobile = function() {
+        // nCollapsed değişkenine mainNavbarı eşledik
+        const nCollapsed = document.body.querySelector("#mainNavbar");
+        //boyle bi etiket bulunmadıysa değişken boşsa return ettitdik
+        if(!nCollapsed) {
+            return;
+        }
+        // ekran y ekseninde yani aşağı doğru bir hareket yoksa bi değişiklik olmasın 
+        else if(window.scrollY === 0) {
+            nCollapsed.classList.remove("navbar-mobile");
+        }
+        //eğer aşağı hareket varsa classı ekledik dedik
+        else {
+            nCollapsed.classList.add("navbar-mobile");
+        }
+    }
+    navbarMobile();
+    // Eğer scrall olayı gerçekleşirsa navbarMobile fonksiyonunu çağır diyoruz
+    document.addEventListener("scroll",navbarMobile)
+
+    const myNavbar = document.querySelector("#mainNavbar");
+    if(myNavbar){
+        new bootstrap.ScrollSpy(document.body,{
+            target:"#mainNavbar",
+            offset: 74,
+
+        })
+    }
+})
+
+var btnCanvas = document.querySelectorAll(".btn-close-canvas");
+for(let i = 0; i < btnCanvas.length; i++) 
+{   
+    btnCanvas[i].addEventListener("click", function (){
+        document.querySelector('[data-bs-toggle="offcanvas"]').click();
+        console.log("asdas")
+    }); 
+}
+
